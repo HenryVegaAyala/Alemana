310df5d398cfb1957c7028f3d1f29fd60a00fac3
HenryVegaAyala <hp.vega@hotmail.com>
2016-05-19 18:30:50 -0500

    Desarrollo Alemana

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,20 +1,31 @@
+
+<center>
+    <h1>Bienvenido a Panadería Pastelería Alemana</h1>
+</center>
+<br>
+
 <?php
-/* @var $this SiteController */
+$this->widget(
+        'booster.widgets.TbCarousel', array(
+    'items' => array(
+        array(
+            'image' => ('images/ms-icon-310x310.png'),
+            'label' => 'First Thumbnail label',
+            'caption' => 'First Caption.'
+        ),
+        array(
+            'image' => ('images/ms-icon-310x310.png'),
+            'label' => 'Second Thumbnail label',
+            'caption' => 'Cras justo odio, dapis ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.'
+        ),
+        array(
+            'image' => ('images/ms-icon-310x310.png'),
+            'label' => 'Third Thumbnail label',
+            'caption' => 'Cras justo odio, dapis ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.'
+        ),
+    ),
+        )
+);
 
-$this->pageTitle=Yii::app()->name;
-?>
-
-<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
-
-<p>Congratulations! You have successfully created your Yii application.</p>
-
-<p>You may change the content of this page by modifying the following two files:</p>
-<ul>
-	<li>View file: <code><?php echo __FILE__; ?></code></li>
-	<li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
-</ul>
-
-<p>For more details on how to further develop this application, please read
-the <a href="http://www.yiiframework.com/doc/">documentation</a>.
-Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
-should you have any questions.</p>
+echo '<br>';
+echo '<br>';
--- protected/views/site/login.php
+++ protected/views/site/login.php
@@ -1,53 +1,59 @@
-<?php
-/* @var $this SiteController */
-/* @var $model LoginForm */
-/* @var $form CActiveForm  */
-
-$this->pageTitle=Yii::app()->name . ' - Login';
-$this->breadcrumbs=array(
-	'Login',
-);
-?>
-
-<h1>Login</h1>
-
-<p>Please fill out the following form with your login credentials:</p>
+<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/login.css">
+<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/animate-custom.css">
 
 <div class="form">
-<?php $form=$this->beginWidget('CActiveForm', array(
-	'id'=>'login-form',
-	'enableClientValidation'=>true,
-	'clientOptions'=>array(
-		'validateOnSubmit'=>true,
-	),
-)); ?>
+    <?php
+    $form = $this->beginWidget('CActiveForm', array(
+        'id' => 'login-form',
+        'enableClientValidation' => true,
+        'clientOptions' => array(
+            'validateOnSubmit' => true,
+        ),
+    ));
+    ?>
 
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
+<div class="container" id="login-block">
+    <div class="row">
+        <div class="col-sm-6 col-md-4 col-sm-offset-3 col-md-offset-4">
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'username'); ?>
-		<?php echo $form->textField($model,'username'); ?>
-		<?php echo $form->error($model,'username'); ?>
-	</div>
+            <div class="login-box clearfix animated flipInY">
+                <div class="login-logo">
+                    <center>
+                        <a href="#"><img align="center" hspace="10" src="<?php echo Yii:: app()->baseUrl . '/images/apple-icon-144x144.png' ?>"></a>
+                    </center>
+                </div> 
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'password'); ?>
-		<?php echo $form->passwordField($model,'password'); ?>
-		<?php echo $form->error($model,'password'); ?>
-		<p class="hint">
-			Hint: You may login with <kbd>demo</kbd>/<kbd>demo</kbd> or <kbd>admin</kbd>/<kbd>admin</kbd>.
-		</p>
-	</div>
+                <div class="login-form">
+                    <div class="alert alert-error hide">
+                        <button type="button" class="close" data-dismiss="alert">&times;</button>
+                        <h4>Error!</h4>
+                        Your Error Message goes here
+                    </div>
 
-	<div class="row rememberMe">
-		<?php echo $form->checkBox($model,'rememberMe'); ?>
-		<?php echo $form->label($model,'rememberMe'); ?>
-		<?php echo $form->error($model,'rememberMe'); ?>
-	</div>
+                    <form  method="post"  >
+                        <?php // echo $form->labelEx($model, 'username'); ?>
+                        <?php echo $form->textField($model, 'username'); ?>
+                        <?php echo $form->error($model, 'username'); ?>
+                        
+                                <?php // echo $form->labelEx($model, 'password'); ?>
+        <?php echo $form->passwordField($model, 'password'); ?>
+        <?php echo $form->error($model, 'password'); ?>
 
-	<div class="row buttons">
-		<?php echo CHtml::submitButton('Login'); ?>
-	</div>
+                        <button type="submit" name="btn-login" class="btn btn-block btn-red">
+                            <i class="glyphicon glyphicon-log-in"></i>&nbsp;Inicia Sesión
+                        </button>
+                        <?php echo CHtml::submitButton('Iniciar Sesión'); ?>
+                         </form>
 
-<?php $this->endWidget(); ?>
-</div><!-- form -->
+                    <div class="login-links"> 
+                        <a href="sign-up.html">¿No tienes cuenta? <strong>INICIA SESIÓN</strong>
+                        </a>
+                    </div>      		
+                </div> 			        	
+            </div>
+        </div>
+    </div>
+</div>
+    <?php $this->endWidget(); ?>
+</div>
