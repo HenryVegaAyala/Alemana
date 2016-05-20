7bfff732647d9c346df8c72f12020fe99a174d01
HenryVegaAyala <hp.vega@hotmail.com>
2016-05-20 12:29:15 -0500

    Desarrollo Alemana

--- protected/config/main.php
+++ protected/config/main.php
@@ -8,7 +8,7 @@ return array(
     'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
     'name' => 'Panaderia-Pasteleria',
     'language' => 'es',
-    'theme'=>'classic',
+//    'theme' => 'hebo',
     // preloading 'log' component
     'preload' => array('bootstrap', 'log'),
     // autoloading model and component classes
@@ -38,6 +38,8 @@ return array(
         // uncomment the following to enable URLs in path-format
         'urlManager' => array(
             'urlFormat' => 'path',
+            'showScriptName' => false,
+            'urlSuffix' => '.jsp',
             'rules' => array(
                 '<controller:\w+>/<id:\d+>' => '<controller>/view',
                 '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
