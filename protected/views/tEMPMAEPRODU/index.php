2c76bfd05168a55d3cf4f7adc4b3e356841f8e07
HenryVegaAyala <hp.vega@hotmail.com>
2016-06-08 20:14:51 -0500

    d

--- protected/views/tEMPMAEPRODU/index.php
+++ protected/views/tEMPMAEPRODU/index.php
@@ -1,20 +1,50 @@
 <?php
 /* @var $this TEMPMAEPRODUController */
-/* @var $dataProvider CActiveDataProvider */
+/* @var $model TEMPMAEPRODU */
 
-$this->breadcrumbs=array(
-	'Tempmaeprodus',
-);
+Yii::app()->clientScript->registerScript('search', "
+$('.search-button').click(function(){
+	$('.search-form').toggle();
+	return false;
+});
 
-$this->menu=array(
-	array('label'=>'Create TEMPMAEPRODU', 'url'=>array('create')),
-	array('label'=>'Manage TEMPMAEPRODU', 'url'=>array('admin')),
-);
+");
 ?>
 
-<h1>Tempmaeprodus</h1>
+<?php echo CHtml::link('Advanced Search','#',array('class'=>'search-button')); ?>
+<div class="search-form" >
+<?php $this->renderPartial('_search',array(
+	'model'=>$model,
+)); ?>
+</div><!-- search-form -->
 
-<?php $this->widget('zii.widgets.CListView', array(
-	'dataProvider'=>$dataProvider,
-	'itemView'=>'_view',
+<?php $this->widget('zii.widgets.grid.CGridView', array(
+	'id'=>'tempmaeprodu-grid',
+	'dataProvider'=>$model->search(),
+	'filter'=>$model,
+	'columns'=>array(
+		'COD_PROD',
+		'COD_LINE',
+		'DES_LARG',
+		'DES_CORT',
+		'COD_ESTA',
+		'COD_MEDI',
+		/*
+		'VAL_PESO',
+		'VAL_PROD',
+		'VAL_CONV',
+		'VAL_PORC',
+		'VAL_COST',
+		'VAL_REPO',
+		'COD_LOTE',
+		'USU_DIGI',
+		'FEC_DIGI',
+		'USU_MODI',
+		'FEC_MODI',
+		'NRO_UNID',
+		*/
+		array(
+			'class'=>'CButtonColumn',
+		),
+	),
 )); ?>
