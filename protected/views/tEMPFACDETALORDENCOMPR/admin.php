63e38274a2be87f5aaeec79850324c10745f7d90
sbuchelli <sergio.buchelli.silva@gmail.com>
2016-06-21 16:49:21 -0500

    focus

--- protected/views/tEMPFACDETALORDENCOMPR/admin.php
+++ protected/views/tEMPFACDETALORDENCOMPR/admin.php
@@ -264,38 +264,5 @@ Yii::app()->session['USU'] = $usuario;
         });
     }
 
-    $('#DES_LARG').autocomplete({
-        source: function(request, response) {
-            $.ajax({
-                url: 'ajax.php',
-                dataType: "json",
-                data: {
-                    nombre_producto: request.term,
-                    type: 'produc_tiend',
-                    row_num: 1
-                },
-                success: function(data) {
-                    response($.map(data, function(item) {
-                        var code = item.split("|");
-                        return {
-                            label: code[0],
-                            value: code[0],
-                            data: item
-                        }
-                    }));
-                }
-            });
-        },
-        autoFocus: true,
-        minLength: 0,
-        select: function(event, ui) {
-            var names = ui.item.data.split("|");
-            console.log(names[1], names[2], names[3]);
-            $('#campo_COD_PROD').val(names[1]);
-            $('#campo_NRO_UNID').val(names[2]);
-            $('#campo_VAL_PREC ').val(names[3]);
-        }
 
-
-    });
 </script>
