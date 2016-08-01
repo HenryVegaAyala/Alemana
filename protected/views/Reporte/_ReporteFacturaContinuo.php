b6fe65f01a5e4292c72719f87ac410e34c3aacc5
HenryVegaAyala <hp.vega@hotmail.com>
2016-08-01 17:25:59 -0500

    REPORTE

--- protected/views/Reporte/_ReporteFacturaContinuo.php
+++ protected/views/Reporte/_ReporteFacturaContinuo.php
@@ -74,7 +74,7 @@ class PDF extends PDF_JavaScript {
         $this->Ln();
 //       -- Espacio 2
         $this->Cell(11.8, 0.5, '', 0, '', 'L');
-        $this->SetTextColor(221, 80, 68);
+        //$this->SetTextColor(221, 80, 68);
         $this->Cell(7.5, 0.5, utf8_decode(strtoupper($Factura)), 0, '', 'L');
         $this->Ln();
 //       -- Espacio 3
