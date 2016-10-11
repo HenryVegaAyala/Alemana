9625c37a4001d6487a2e051ac2f7d5449c4238a8
HenryVegaAyala <hp.vega@hotmail.com>
2016-10-11 10:48:17 -0500

    Alemana v2

--- protected/config/main.php
+++ protected/config/main.php
@@ -5,31 +5,31 @@
 // This is the main Web application configuration. Any writable
 // CWebApplication properties can be configured here.
 return array(
-    'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
+	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
     'name' => 'Panaderia-Pasteleria',
     'language' => 'es',
 //    'theme' => 'hebo',
-    // preloading 'log' component
+	// preloading 'log' component
     'preload' => array('bootstrap', 'log'),
-    // autoloading model and component classes
-    'import' => array(
-        'application.models.*',
-        'application.components.*',
-    ),
-    'modules' => array(
-        'gii' => array(
-            'class' => 'system.gii.GiiModule',
+	// autoloading model and component classes
+	'import'=>array(
+		'application.models.*',
+		'application.components.*',
+	),
+	'modules'=>array(
+		'gii'=>array(
+			'class'=>'system.gii.GiiModule',
             'password' => 'alemana',
-            // If removed, Gii defaults to localhost only. Edit carefully to taste.
-            'ipFilters' => array('127.0.0.1', '::1'),
-        ),
+			// If removed, Gii defaults to localhost only. Edit carefully to taste.
+			'ipFilters'=>array('127.0.0.1','::1'),
+		),
         'generatorPaths' => array('bootstrap.gii')
-    ),
-    // application components
-    'components' => array(
-        'user' => array(
-            // enable cookie-based authentication
-            'allowAutoLogin' => true,
+	),
+	// application components
+	'components'=>array(
+		'user'=>array(
+			// enable cookie-based authentication
+			'allowAutoLogin'=>true,
             'loginUrl' => array('site/login'),
         ),
         'session' => array(
@@ -41,44 +41,44 @@ return array(
         'bootstrap' => array(
             'class' => 'ext.bootstrap.components.Booster',
             'responsiveCss' => true,
-        ),
-        // uncomment the following to enable URLs in path-format
-        'urlManager' => array(
-            'urlFormat' => 'path',
+		),
+		// uncomment the following to enable URLs in path-format
+		'urlManager'=>array(
+			'urlFormat'=>'path',
             'showScriptName' => false,
             'urlSuffix' => '.php',
-            'rules' => array(
-                '<controller:\w+>/<id:\d+>' => '<controller>/view',
-                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
-                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
-            ),
-        ),
-        // database settings are configured in database.php
-        'db' => require(dirname(__FILE__) . '/database.php'),
-        'errorHandler' => array(
-            // use 'site/error' action to display errors
-            'errorAction' => YII_DEBUG ? null : 'site/error',
-        ),
-        'log' => array(
-            'class' => 'CLogRouter',
-            'routes' => array(
-                array(
-                    'class' => 'CFileLogRoute',
-                    'levels' => 'error, warning',
-                ),
-            // uncomment the following to show log messages on web pages
-            /*
-              array(
-              'class'=>'CWebLogRoute',
-              ),
-             */
-            ),
-        ),
-    ),
-    // application-level parameters that can be accessed
-    // using Yii::app()->params['paramName']
-    'params' => array(
-        // this is used in contact page
-        'adminEmail' => 'webmaster@example.com',
-    ),
+			'rules'=>array(
+				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
+				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
+				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+			),
+		),
+		// database settings are configured in database.php
+		'db'=>require(dirname(__FILE__).'/database.php'),
+		'errorHandler'=>array(
+			// use 'site/error' action to display errors
+			'errorAction'=>YII_DEBUG ? null : 'site/error',
+		),
+		'log'=>array(
+			'class'=>'CLogRouter',
+			'routes'=>array(
+				array(
+					'class'=>'CFileLogRoute',
+					'levels'=>'error, warning',
+				),
+				// uncomment the following to show log messages on web pages
+				/*
+				array(
+					'class'=>'CWebLogRoute',
+				),
+				*/
+			),
+		),
+	),
+	// application-level parameters that can be accessed
+	// using Yii::app()->params['paramName']
+	'params'=>array(
+		// this is used in contact page
+		'adminEmail'=>'webmaster@example.com',
+	),
 );
