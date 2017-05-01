e134d3a59eba51c68f070ae3fd9bfe559a578656
HenryVegaAyala <hp.vega@hotmail.com>
2017-05-01 10:30:17 -0500

    Ajustes

--- protected/config/database.php
+++ protected/config/database.php
@@ -2,21 +2,21 @@
 
 // localhost
 
-return array(
-    'connectionString' => 'mysql:host=localhost;port=3306;dbname=SIS_PANA',
-	'emulatePrepare' => true,
-	'username' => 'root',
-	'password' => '',
-	'charset' => 'utf8',
-);
+//return array(
+//    'connectionString' => 'mysql:host=localhost;port=3306;dbname=SIS_PANA',
+//	'emulatePrepare' => true,
+//	'username' => 'root',
+//	'password' => '',
+//	'charset' => 'utf8',
+//);
 
 
 // remoto aws
 
-//return array(
-//	'connectionString' => 'mysql:host=sispaal.cnjv4vhhy3or.us-west-2.rds.amazonaws.com;port=3306;dbname=SIS_PANA',
-//	'emulatePrepare' => true,
-//	'username' => 'root',
-//	'password' => 'root2016',
-//	'charset' => 'utf8',
-//);
+return array(
+	'connectionString' => 'mysql:host=sispaal.cnjv4vhhy3or.us-west-2.rds.amazonaws.com;port=3306;dbname=SIS_PANA',
+	'emulatePrepare' => true,
+	'username' => 'root',
+	'password' => 'root2016',
+	'charset' => 'utf8',
+);
